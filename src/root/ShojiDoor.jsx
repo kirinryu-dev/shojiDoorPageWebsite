d4c127ab817ecea480530cdf5f4b1a28c8362ab7
kirinryu.dev <kirinryu.dev@gmail.com>
2024-07-05 09:27:41 +0000

    another day another commit

--- src/root/ShojiDoor.jsx
+++ src/root/ShojiDoor.jsx
@@ -1,9 +1,11 @@
+// src/components/ShojiDoor.js
 import React, { useState } from 'react';
 import NavBar from '../components/NavigationBar';
 import panelData from '../data';
 import './ShojiDoors.css';
 import Modal from '../pages/Modal'; // Import your Modal component
 
+// Example component imports (using alias names for clarity)
 import Panel1 from '../components/Pannel1'; // Adjust the import paths as per your project structure
 import Panel2 from '../components/Pannel2';
 import Panel3 from '../components/Pannel3';
@@ -12,28 +14,32 @@ import Panel4 from '../components/Pannel4';
 const ShojiDoor = () => {
   const [activePanel, setActivePanel] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [panelState, setPanelState] = useState('idle');
 
   const handlePanelClick = (panel) => {
     setActivePanel(panel.id);
     setIsModalOpen(true);
-    setPanelState('clicked');
+
+    // Trigger the door opening animation
+    document.querySelectorAll('.shoji_panel').forEach((panelElement, index) => {
+      if (index < 2) {
+        panelElement.classList.add('active');
+      } else {
+        panelElement.classList.add('active');
+      }
+    });
   };
 
   const handleCloseModal = () => {
     setActivePanel(null);
     setIsModalOpen(false);
-    setPanelState('idle');
+
+    // Reset the door animation
+    document.querySelectorAll('.shoji_panel').forEach(panelElement => {
+      panelElement.classList.remove('active');
+    });
   };
 
-  const handleMouseEnter = () => {
-    setPanelState('hover');
-  };
-
-  const handleMouseLeave = () => {
-    setPanelState('idle');
-  };
-
+  // Function to render component based on activePanel state
   const renderModalContent = () => {
     switch (activePanel) {
       case 1:
@@ -56,11 +62,9 @@ const ShojiDoor = () => {
         {panelData.map((panel) => (
           <div
             key={panel.id}
-            className={`shoji_panel ${panelState}`}
+            className={`shoji_panel ${activePanel === panel.id ? 'active' : ''}`}
             style={{ backgroundImage: `url(${panel.bgImage})` }}
             onClick={() => handlePanelClick(panel)}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
           >
             <div className="panel_title">{panel.title}</div>
           </div>
